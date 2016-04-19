8f2cd10ec40e2152fac9ff7e7b79be1db186b589
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2016-04-19 17:31:59 +0800

    cors (need server coop)

--- wikimap.js
+++ wikimap.js
@@ -8,8 +8,10 @@ function init() {
     G.baseLayer = L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(G.theMap);
     G.gjLayer = L.geoJson().addTo(G.theMap);
 
-    $.getJSON('ex1.geojson', addGeojsonLayer);
-    // $.get('ex1.geojson').done(addGeojsonLayer);
+    // $.getJSON('ex1.geojson', addGeojsonLayer);
+    $.getJSON('http://v.im.cyut.edu.tw/~ckhung/m/14/osmand/favourites.geojson', addGeojsonLayer);
+    // http://enable-cors.org/
+    // https://stackoverflow.com/questions/11281895/jquery-ajax-and-getjson-requests-hitting-access-control-allow-origin-issues
 }
 
 function addGeojsonLayer(data) {
